a058f8eb56e6439000e0983c501206a52f384925
Joshua Chapman <jwchapmansocial@gmail.com>
2018-08-30 04:03:59 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,50 @@
+var chocolateBars = [
+  'snickers', 
+  'hundred grand', 
+  'kitkat', 
+  'skittles'
+  ];
+  
+  function addElementToBeginningOfArray(chocolateBars, foo) {
+    var newChocolateBars = [foo, ...chocolateBars];
+    return newChocolateBars;
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(chocolateBars, foo) {
+    chocolateBars.unshift(foo);
+    return chocolateBars;
+  }
+  
+  function addElementToEndOfArray(chocolateBars, element) {
+    var newerChocolateBars = [...chocolateBars, element]
+    return newerChocolateBars
+  }
+  
+  function destructivelyAddElementToEndOfArray (chocolateBars, foo) {
+    chocolateBars.push(foo)
+    return chocolateBars
+  }
+  
+  function accessElementInArray (chocolateBars, index) {
+    return chocolateBars[2]
+    }
+  
+  function destructivelyRemoveElementFromBeginningOfArray (chocolateBars) {
+    chocolateBars.shift()
+    return chocolateBars
+  }
+  
+  function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
+    chocolateBars.pop()
+    return chocolateBars
+  }
+  
+  function removeElementFromBeginningOfArray (chocolateBars) {
+    chocolateBars.slice(0)
+    return chocolateBars
+  }
+  
+  function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
+    chocolateBars.pop()
+    return chocolateBars
+  }
